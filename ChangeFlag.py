e8271835477b74d534552a295ce4d5b8f91794f8
mbao <mbao1996@hotmail.com>
2019-07-04 10:39:01 +0800

    add flag grade

--- ChangeFlag.py
+++ ChangeFlag.py
@@ -7,30 +7,33 @@ ShareBank = read_data(fn)
 
 flag_before = 'no'
 flag_after = 'holding'
-to_add = ['', 'goodu']
-to_del = ['', 'holding']
+to_add = ['600395', 'grade', 3]
+to_del = ['code', 'holding']
 batch_add = 'attention'
 
 count = 0
 for i in range( len(ShareBank) ):
     s = ShareBank[i]
+    # change
     if( flag_before in s.flag ):
         count += 1
         s.flag[flag_after] = s.flag[flag_before]
         del s.flag[flag_before]
         print(i, '[max:', len(ShareBank), ']: ', s.nmcard(), '---flag---:', s.flag)
         print('   ', s.dt,'\n   ', s.rt,'\n   ', s.cp)
+    # add
     if( s.id == to_add[0] ):
         count += 1
-        s.flag[to_add[1]] = 'Y'
+        s.flag[to_add[1]] = to_add[2]
         print(i, '[max:', len(ShareBank), ']: ', s.nmcard(), '---flag---:', s.flag)
         print('   ', s.dt,'\n   ', s.rt,'\n   ', s.cp)
+    # delete
     if( s.id == to_del[0] and to_del[1] in s.flag ):
         count += 1
         del s.flag[to_del[1]]
         print(i, '[max:', len(ShareBank), ']: ', s.nmcard(), '---flag---:', s.flag)
         print('   ', s.dt,'\n   ', s.rt,'\n   ', s.cp)
-
+    # batch add
     batch_add_flag = False
 #    batch_add_flag = 'holding' in s.flag or 'goodu' in s.flag
     if(batch_add_flag):
