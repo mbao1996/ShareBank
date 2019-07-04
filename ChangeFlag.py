4b53755dfb1a2a9b83debefbc1e22c92f3790a3f
mbao <mbao1996@hotmail.com>
2019-07-04 09:03:17 +0800

    add batch_add function

--- ChangeFlag.py
+++ ChangeFlag.py
@@ -7,8 +7,9 @@ ShareBank = read_data(fn)
 
 flag_before = 'no'
 flag_after = 'holding'
-to_add = ['600177', 'goodu']
-to_del = ['002110', 'holding']
+to_add = ['', 'goodu']
+to_del = ['', 'holding']
+batch_add = 'attention'
 
 count = 0
 for i in range( len(ShareBank) ):
@@ -29,6 +30,14 @@ for i in range( len(ShareBank) ):
         del s.flag[to_del[1]]
         print(i, '[max:', len(ShareBank), ']: ', s.nmcard(), '---flag---:', s.flag)
         print('   ', s.dt,'\n   ', s.rt,'\n   ', s.cp)
+
+    batch_add_flag = False
+#    batch_add_flag = 'holding' in s.flag or 'goodu' in s.flag
+    if(batch_add_flag):
+        count += 1
+        s.flag[batch_add] = 'Y'
+        print(i, '[max:', len(ShareBank), ']: ', s.nmcard(), '---flag---:', s.flag)
+
 print(count)        
 save_data(fn, ShareBank)
 print('\n finished')
