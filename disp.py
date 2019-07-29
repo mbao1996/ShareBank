3ad91e3a3cb76ded6238fa89e1ee8eb7cc7f8005
mbao1996 <mbao1996@hotmail.com>
2019-07-29 10:32:07 +0800

    change disp.py

--- disp.py
+++ disp.py
@@ -4,6 +4,7 @@ from lib import *
 
 fn = work_catalog + bank_name
 ShareBank = read_data(fn)
+flag = ['holding']
 
 row_start = 0             # adjustable
 row_end = 4000            # adjustable
@@ -12,7 +13,8 @@ if( row_end > len(ShareBank) ):
     row_end = len(ShareBank)
 for i in range( row_start, row_end ):
     s = ShareBank[i]
-    if( s.dt['profit_dedt_qtrs'][0] == last_qtr(get_today()) ):
+    if( has_flags(s, flag) ):
+#    if( s.dt['profit_dedt_qtrs'][0] == last_qtr(get_today()) ):
         print(i, '[max:', len(ShareBank), ']: ', s.nmcard(), '---flag---:', s.flag)
         print('   ', s.dt,'\n   ', s.rt,'\n   ', s.cp)
 
